e4925ed5d61ac9d1e3cbcf76d74a9292892fc55c
Dylan Jordan <dylanpeterjordan@gmail.com>
2021-11-24 15:05:42 -0500

    added damage when collided with enemies

---
+++ Assets/Scripts/game-mechanics/Player.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D playerBody;
+
+    public int _maxHealth = 8;
+    public int _currentHealth;
+
+    public HealthBar healthBarUI;
+
+    void Start()
+    {
+        _currentHealth = _maxHealth;
+        healthBarUI.SetMaxHealth(_maxHealth);
+    }
+
+    void Update()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TakeDamage(1);
+        }
+        if (_currentHealth == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void TakeDamage(int damage)
+    {
+        _currentHealth -= damage;
+        healthBarUI.SetHealth(_currentHealth);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Dragon")
+        {
+            TakeDamage(2);
+            playerBody.velocity = -playerBody.velocity * 100f;
+        }
+
+        if (collision.gameObject.tag == "Bug")
+        {
+            TakeDamage(1);
+            playerBody.velocity = -playerBody.velocity * 100f;
+        }
+    }
+}
