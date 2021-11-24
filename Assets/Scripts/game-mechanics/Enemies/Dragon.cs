bac59fa25a8132853d35af9bc3f7d776fff25d07
Dylan Jordan <dylanpeterjordan@gmail.com>
2021-11-24 16:31:38 -0500

    merged complete lol

--- Assets/Scripts/game-mechanics/Enemies/Dragon.cs
+++ Assets/Scripts/game-mechanics/Enemies/Dragon.cs
@@ -7,17 +7,12 @@ public class Dragon : MonoBehaviour
     Player player;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        player.TakeDamage(2);
     }
 }
