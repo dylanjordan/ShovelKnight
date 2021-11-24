b445b37bafeca37317aba8645b88f43a05a2edbf
William Heath <william.heath@ontariotechu.net>
2021-11-24 15:05:23 -0500

    added some bug code

--- Assets/Scripts/game-mechanics/Enemies/Bugs.cs
+++ Assets/Scripts/game-mechanics/Enemies/Bugs.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Bugs : MonoBehaviour
 {
+
+    [SerializeField] float horizontalMoveSpeed;
+    [SerializeField] Transform cameraTrans;
+
+    Rigidbody2D body;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        body.velocity = new Vector2(-horizontalMoveSpeed, body.velocity.y);
     }
 }
