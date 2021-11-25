f9e6b3b9b0ed1fc41474a6a5f2e0d43c3ac60574
Dylan Jordan <dylanpeterjordan@gmail.com>
2021-11-25 17:41:24 -0500

    added ladder function

--- Assets/Scripts/game-mechanics/Shovel Knight/PlayerController.cs
+++ Assets/Scripts/game-mechanics/Shovel Knight/PlayerController.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Transform trans;
-    Rigidbody2D body;
+    public Rigidbody2D body;
 
     [SerializeField] float speed;
     [SerializeField] float jumpForce;
 
-    bool jumpInput;
-    bool isGrounded;
+    private bool jumpInput;
+    private bool isGrounded;
+    private bool isClimbing;
+
+    private float playerGravity = 7f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,17 +26,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Walk();
-
-        if (Input.GetKeyDown(KeyCode.W))
+        if (!isClimbing)
         {
-            jumpInput = true;
-        }
-        if (Input.GetKeyUp(KeyCode.W))
-        {
-            jumpInput = false;
-        }
+            Walk();
 
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                jumpInput = true;
+            }
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                jumpInput = false;
+            }
+        }
+        if (isClimbing)
+        {
+            body.gravityScale = 0;
+            body.drag = 50;
+            ClimbMovement();
+        }
+        else
+        {
+            body.gravityScale = playerGravity;
+            body.drag = 0;
+        }
     }
 
     void FixedUpdate()
@@ -66,6 +82,27 @@ public class PlayerController : MonoBehaviour
         isGrounded = false;
         SoundManager.PlaySound("jumpSound");
     }
+    void ClimbMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            trans.position += transform.up * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            trans.position -= transform.up * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            trans.position += transform.right * Time.deltaTime * speed;
+            trans.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            trans.position += transform.right * Time.deltaTime * speed;
+            trans.rotation = Quaternion.Euler(0, 180, 0);
+        }
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -80,7 +117,20 @@ public class PlayerController : MonoBehaviour
             }
         }
     }
-
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            isClimbing = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            isClimbing = false;
+        }
+    }
     public float GetSpeed()
     {
         return speed;
