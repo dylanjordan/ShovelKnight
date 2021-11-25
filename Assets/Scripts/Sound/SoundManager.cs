f8c1ab285fd268ddf77243faff519d9a25b2658f
Dylan Jordan <dylanpeterjordan@gmail.com>
2021-11-25 13:16:42 -0500

    added sound to main

---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static AudioClip playerHitSound, inGameBackgroundSound, jumpSound;
+    static AudioSource audioSrc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inGameBackgroundSound = Resources.Load<AudioClip>("Menu Sound #1");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            case "GameLoaded":
+                audioSrc.PlayOneShot(inGameBackgroundSound);
+                break;
+        }
+    }
+}
