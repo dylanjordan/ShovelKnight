85b3c21f86ac06b45aaabb0b5e72c93e0098533b
William Heath <william.heath@ontariotechu.net>
2021-12-06 03:01:05 -0500

    Bug fix

--- Assets/Scripts/game-mechanics/Shovel Knight/PlayerController.cs
+++ Assets/Scripts/game-mechanics/Shovel Knight/PlayerController.cs
@@ -108,10 +108,12 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             trans.position += transform.right * Time.deltaTime * speed;
+            trans.rotation = Quaternion.Euler(0, 0, 0);
         }
         if (Input.GetKey(KeyCode.A))
         {
             trans.position += transform.right * Time.deltaTime * speed;
+            trans.rotation = Quaternion.Euler(0, 180, 0);
         }
     }
 
